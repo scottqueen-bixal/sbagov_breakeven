1acfce0cddc8aad1e31ac3d97b4819b7ebb48c6e
gscottqueen <scott.queen@bixal.com>
2024-08-08 11:08:26 -0400

    Include custom block

---
+++ drupal/custom/sbagov_breakeven/Block/SbagovBreakevenBlock.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\sbagov_breakeven\Plugin\Block;
+
+use Drupal\Core\Block\Attribute\Block;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+
+/**
+ * Provides a sbagov_breakeven block.
+ */
+#[Block(
+  id: 'sbagov_breakeven',
+  admin_label: new TranslatableMarkup('sbagov_breakeven'),
+  category: new TranslatableMarkup('Custom'),
+)]
+final class SbagovBreakevenBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array {
+    $build['content'] = [
+      '#markup' => $this->t('It works!'),
+    ];
+    return $build;
+  }
+
+}
