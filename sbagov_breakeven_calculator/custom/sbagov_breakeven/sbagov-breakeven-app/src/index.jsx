85101bea84c0a5c617266c4f8bc2c270d85f7d8b
gscottqueen <scott.queen@bixal.com>
2024-08-20 15:44:23 -0400

    remove unused heading on root component

--- sbagov_breakeven_calculator/custom/sbagov_breakeven/sbagov-breakeven-app/src/index.jsx
+++ sbagov_breakeven_calculator/custom/sbagov_breakeven/sbagov-breakeven-app/src/index.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import BreakEvenCalculator from "./pages/index";
+import "../node_modules/semantic-ui-less/semantic.less";
 
 createRoot(document.getElementById("sbagov-breakeven")).render(
   <StrictMode>
-    <h1>sba_breakeven REACT Root</h1>
     <BreakEvenCalculator />
   </StrictMode>
 );
