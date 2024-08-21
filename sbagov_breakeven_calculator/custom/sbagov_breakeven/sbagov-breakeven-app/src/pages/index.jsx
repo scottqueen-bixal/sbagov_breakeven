2c0339a25497cc53c5fe71553365fb9604e78667
gscottqueen <scott.queen@bixal.com>
2024-08-21 10:41:13 -0400

    migrate to functional component

--- sbagov_breakeven_calculator/custom/sbagov_breakeven/sbagov-breakeven-app/src/pages/index.jsx
+++ sbagov_breakeven_calculator/custom/sbagov_breakeven/sbagov-breakeven-app/src/pages/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid, Container } from "semantic-ui-react";
 import Layout from "../components/layout";
 import { EditableTotal } from "../components/atoms";
@@ -11,156 +11,140 @@ import {
   VariableCosts,
 } from "../components/organisms";
 import { CALCULATOR_STEPS, FAQ_CONTENT } from "../constants";
+
 import "../styles/typography.less";
 import "./index.less";
 
-class BreakEvenCalculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      stepNum: 0,
-      variableCostPerUnit: "",
-      numUnits: "",
-      pricePerUnit: "",
-      totalFixedCost: "",
-      shouldReset: false,
-    };
-  }
+const BreakEvenCalculator = () => {
+  const [stepNum, setStepNum] = useState(0);
+  const [variableCostPerUnit, setVariableCostPerUnit] = useState("");
+  const [numUnits, setNumUnits] = useState("");
+  const [pricePerUnit, setPerPriceUnit] = useState("");
+  const [totalFixedCost, setTotalFixedCost] = useState("");
+  const [shouldReset, setShouldReset] = useState(false);
 
-  goToStep = (stepNum) => {
-    this.setState({ stepNum });
-  };
+  console.log(
+    { stepNum },
+    { variableCostPerUnit },
+    { numUnits },
+    { pricePerUnit },
+    { totalFixedCost },
+    { shouldReset }
+  );
 
-  updateVariableCost = (variableCost) => {
-    this.setState({ variableCostPerUnit: variableCost });
-  };
+  const goToStep = (stepNum) => setStepNum(stepNum);
 
-  updateNumUnits = (numUnits) => {
-    this.setState({ numUnits });
-  };
+  const updateVariableCost = (variableCost) =>
+    setVariableCostPerUnit(variableCost);
 
-  updatePricePerUnit = (price) => {
-    this.setState({ pricePerUnit: price });
-  };
+  const updateNumUnits = (numUnits) => setNumUnits(numUnits);
 
-  updateFixedCost = (fixedCost) => {
-    this.setState({ totalFixedCost: fixedCost });
-  };
+  const updatePricePerUnit = (price) => setPerPriceUnit(price);
 
-  restartAnalysis = () => {
-    this.setState({ shouldReset: true }, () => {
-      this.setState({
-        stepNum: 0,
-        variableCostPerUnit: "",
-        numUnits: "",
-        pricePerUnit: "",
-        totalFixedCost: "",
-        shouldReset: false,
-      });
-    });
-    this.goToStep(CALCULATOR_STEPS.FIXED_COSTS);
-  };
+  const updateFixedCost = (fixedCost) => setTotalFixedCost(fixedCost);
 
-  render() {
-    if (this.state.stepNum === CALCULATOR_STEPS.RESULTS_PAGE) {
-      return (
-        <Layout>
-          <Results
-            variableCostPerUnit={this.state.variableCostPerUnit || 0}
-            numUnits={this.state.numUnits || 0}
-            pricePerUnit={this.state.pricePerUnit || 0}
-            totalFixedCost={this.state.totalFixedCost || 0}
-            updateFixedCost={this.updateFixedCost}
-            updateNumUnits={this.updateNumUnits}
-            updatePricePerUnit={this.updatePricePerUnit}
-            updateVariableCost={this.updateVariableCost}
-          />
-        </Layout>
-      );
+  const restartAnalysis = () => {
+    if (shouldReset === true) {
+      setStepNum(0);
+      setVariableCostPerUnit("");
+      setNumUnits("");
+      setPricePerUnit("");
+      setTotalFixedCost("");
+      setShouldReset(false);
+      goToStep(CALCULATOR_STEPS.FIXED_COSTS);
     }
-    return (
-      <Layout>
-        <Grid columns={1}>
-          <Grid.Column>
-            <Hero>
-              <>
-                <FixedCosts
-                  visible={this.state.stepNum === CALCULATOR_STEPS.FIXED_COSTS}
-                  goToStep={this.goToStep}
-                  setFixedCost={this.updateFixedCost}
-                  totalFixedCosts={this.state.totalFixedCost}
-                  key={this.state.shouldReset} // change in key forces a re-mount
-                />
-                <UnitSales
-                  visible={this.state.stepNum === CALCULATOR_STEPS.UNIT_SALES}
-                  goToStep={this.goToStep}
-                  setNumUnits={this.updateNumUnits}
-                  value={this.state.numUnits}
-                  restart={this.restartAnalysis}
-                />
-                <PricePerUnit
-                  visible={
-                    this.state.stepNum === CALCULATOR_STEPS.PRICE_PER_UNIT
-                  }
-                  goToStep={this.goToStep}
-                  setUnitPrice={this.updatePricePerUnit}
-                  value={this.state.pricePerUnit}
-                  restart={this.restartAnalysis}
-                />
-                <VariableCosts
-                  visible={
-                    this.state.stepNum === CALCULATOR_STEPS.VARIABLE_COSTS
-                  }
-                  pricePerUnit={this.state.pricePerUnit}
-                  goToStep={this.goToStep}
-                  setVariableCost={this.updateVariableCost}
-                  restart={this.restartAnalysis}
-                  key={this.state.shouldReset + 1} // change in key forces a re-mount
-                />
-              </>
-            </Hero>
-          </Grid.Column>
+  };
+
+  return stepNum === CALCULATOR_STEPS.RESULTS_PAGE ? (
+    <Layout>
+      <Results
+        variableCostPerUnit={variableCostPerUnit || 0}
+        numUnits={numUnits || 0}
+        pricePerUnit={pricePerUnit || 0}
+        totalFixedCost={totalFixedCost || 0}
+        updateFixedCost={updateFixedCost}
+        updateNumUnits={updateNumUnits}
+        updatePricePerUnit={updatePricePerUnit}
+        updateVariableCost={updateVariableCost}
+      />
+    </Layout>
+  ) : (
+    <Layout>
+      <Grid columns={1}>
+        <Grid.Column>
+          <Hero>
+            <>
+              <FixedCosts
+                visible={stepNum === CALCULATOR_STEPS.FIXED_COSTS}
+                goToStep={goToStep}
+                setFixedCost={updateFixedCost}
+                totalFixedCost={totalFixedCost}
+                key={shouldReset} // change in key forces a re-mount
+              />
+              <UnitSales
+                visible={stepNum === CALCULATOR_STEPS.UNIT_SALES}
+                goToStep={goToStep}
+                setNumUnits={updateNumUnits}
+                value={numUnits}
+                restart={restartAnalysis}
+              />
+              <PricePerUnit
+                visible={stepNum === CALCULATOR_STEPS.PRICE_PER_UNIT}
+                goToStep={goToStep}
+                setUnitPrice={updatePricePerUnit}
+                value={pricePerUnit}
+                restart={restartAnalysis}
+              />
+              <VariableCosts
+                visible={stepNum === CALCULATOR_STEPS.VARIABLE_COSTS}
+                pricePerUnit={pricePerUnit}
+                goToStep={goToStep}
+                setVariableCost={updateVariableCost}
+                restart={restartAnalysis}
+                key={shouldReset + 1} // change in key forces a re-mount
+              />
+            </>
+          </Hero>
+        </Grid.Column>
+      </Grid>
+      <Container className="runningTotals-container">
+        <Grid>
+          {stepNum > CALCULATOR_STEPS.UNIT_SALES && (
+            <EditableTotal
+              key="number of units"
+              title="Number of units"
+              type="units"
+              value={numUnits}
+              onEdit={updateNumUnits}
+            />
+          )}
+          {stepNum > CALCULATOR_STEPS.PRICE_PER_UNIT && (
+            <EditableTotal
+              key="price per unit"
+              title="Selling price per unit"
+              value={pricePerUnit}
+              onEdit={updatePricePerUnit}
+            />
+          )}
+          {stepNum > CALCULATOR_STEPS.FIXED_COSTS && (
+            <EditableTotal
+              key="total fixed cost"
+              title="Total fixed cost"
+              value={totalFixedCost}
+              onEdit={updateFixedCost}
+            />
+          )}
         </Grid>
-        <Container className="runningTotals-container">
-          <Grid>
-            {this.state.stepNum > CALCULATOR_STEPS.UNIT_SALES && (
-              <EditableTotal
-                key="number of units"
-                title="Number of units"
-                type="units"
-                value={this.state.numUnits}
-                onEdit={this.updateNumUnits}
-              />
-            )}
-            {this.state.stepNum > CALCULATOR_STEPS.PRICE_PER_UNIT && (
-              <EditableTotal
-                key="price per unit"
-                title="Selling price per unit"
-                value={this.state.pricePerUnit}
-                onEdit={this.updatePricePerUnit}
-              />
-            )}
-            {this.state.stepNum > CALCULATOR_STEPS.FIXED_COSTS && (
-              <EditableTotal
-                key="total fixed cost"
-                title="Total fixed cost"
-                value={this.state.totalFixedCost}
-                onEdit={this.updateFixedCost}
-              />
-            )}
-          </Grid>
-        </Container>
-        <Container>
-          <BecAccordion data={FAQ_CONTENT[this.state.stepNum] || []} />
-        </Container>
-        <feedback-form
-          product="BEPC"
-          productTitle="COVID Break Even Point Calculator Feedback"
-        ></feedback-form>
-        ,
-      </Layout>
-    );
-  }
-}
+      </Container>
+      <Container>
+        <BecAccordion data={FAQ_CONTENT[stepNum] || []} />
+      </Container>
+      <feedback-form
+        product="BEPC"
+        productTitle="COVID Break Even Point Calculator Feedback"
+      ></feedback-form>
+    </Layout>
+  );
+};
 
 export default BreakEvenCalculator;
