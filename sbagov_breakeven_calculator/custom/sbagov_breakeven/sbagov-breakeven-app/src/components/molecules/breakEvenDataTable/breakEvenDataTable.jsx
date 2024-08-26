f51be69178c305f0cea541076a612c5484905e4b
scottqueen-bixal <scott.queen@bixal.com>
2024-08-26 10:32:13 -0400

    migrate from class to functional component

--- sbagov_breakeven_calculator/custom/sbagov_breakeven/sbagov-breakeven-app/src/components/molecules/breakEvenDataTable/breakEvenDataTable.jsx
+++ sbagov_breakeven_calculator/custom/sbagov_breakeven/sbagov-breakeven-app/src/components/molecules/breakEvenDataTable/breakEvenDataTable.jsx
@@ -1,130 +1,128 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-import { Card, Table } from 'semantic-ui-react'
-import { formatNumber } from '../../../helpers'
+import { Card, Table } from "semantic-ui-react";
+import { formatNumber } from "../../../helpers";
 
-import './breakEvenDataTable.less'
+import "./breakEvenDataTable.less";
 
-class BreakEvenDataTable extends React.Component {
-	state = {
-    column: null,
-    direction: null,
-  }
+const BreakEvenDataTable = (props) => {
+  const [column, setColumn] = useState("");
+  const [direction, setDirection] = useState("ascending");
+  const { data } = props;
 
-  handleSort = (clickedColumn) => () => {
-    const { column, direction } = this.state
-    const { data } = this.props
-
+  const handleSort = (clickedColumn) => () => {
     if (column !== clickedColumn) {
-      this.setState({
-        column: clickedColumn,
-        data: data.sort(function(a, b){return a.clickedColumn - b.clickedColumn}),
-        direction: 'ascending',
-      })
-
-      return
+      setColumn(clickedColumn);
+      data.sort(function (a, b) {
+        return a.clickedColumn - b.clickedColumn;
+      });
+      setDirection("ascending");
+      return;
     }
 
-    this.setState({
-      data: data.reverse(),
-      direction: direction === 'ascending' ? 'descending' : 'ascending',
-    })
-	}
+    data.reverse();
+    setDirection(direction === "ascending" ? "descending" : "ascending");
+  };
 
-	formatProfitOrLoss = (profit) => {
-		if (profit >= 0) return `$${formatNumber(profit)}`
-		return <span>&minus;{`$${formatNumber(Math.abs(profit))}`}</span>
-	}
+  const formatProfitOrLoss = (profit) => {
+    if (profit >= 0) return `$${formatNumber(profit)}`;
+    return <span>&minus;{`$${formatNumber(Math.abs(profit))}`}</span>;
+  };
 
-  render() {
-    const { column, direction } = this.state
-    const { data } = this.props
-    const uniqueKey = () => {
-      if(isNaN(data.units)) {
-        return `${data[1].units}-${data[1].profit}-${data[1].revenue}-${data[1].variableCosts}-${data[1].fixedCosts}`
-      }
-      return `${Math.floor(Math.random() * 100)}`;
+  const uniqueKey = () => {
+    if (isNaN(data.units)) {
+      return `${data[1].units}-${data[1].profit}-${data[1].revenue}-${data[1].variableCosts}-${data[1].fixedCosts}`;
     }
+    return `${Math.floor(Math.random() * 100)}`;
+  };
 
-    return (
-      <Card fluid>
-        <Card.Content className='bep-table-overflow'>
-          <h3>Break-Even Point Unit Sales</h3>
-          <Table data-testid='bep-dataTable' id='bep-dataTable' textAlign='right' sortable unstackable >
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell
-                  sorted={column === 'units' ? direction : null}
-                  onClick={this.handleSort('units')}
+  return (
+    <Card fluid>
+      <Card.Content className="bep-table-overflow">
+        <h3>Break-Even Point Unit Sales</h3>
+        <Table
+          data-testid="bep-dataTable"
+          id="bep-dataTable"
+          textAlign="right"
+          sortable
+          unstackable
+        >
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell
+                sorted={column === "units" ? direction : null}
+                onClick={handleSort("units")}
+              >
+                Units Sold
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === "profit" ? direction : null}
+                onClick={handleSort("profit")}
+              >
+                Profit
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === "revenue" ? direction : null}
+                onClick={handleSort("revenue")}
+              >
+                Unit Sales
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === "variableCosts" ? direction : null}
+                onClick={handleSort("variableCosts")}
+              >
+                Variable Costs
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === "fixedCosts" ? direction : null}
+                onClick={handleSort("fixedCosts")}
+              >
+                Fixed Costs
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === "totalCosts" ? direction : null}
+                onClick={handleSort("totalCosts")}
+              >
+                Total Costs
+              </Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body key={uniqueKey()}>
+            {data.map(
+              ({
+                units,
+                profit,
+                revenue,
+                variableCosts,
+                fixedCosts,
+                totalCosts,
+              }) => (
+                <Table.Row
+                  data-testid="data-table-row"
+                  key={`${profit}-${revenue}-${variableCosts}`}
                 >
-                  Units Sold
-                </Table.HeaderCell>
-                <Table.HeaderCell
-                  sorted={column === 'profit' ? direction : null}
-                  onClick={this.handleSort('profit')}
-                >
-                  Profit
-                </Table.HeaderCell>
-                <Table.HeaderCell
-                  sorted={column === 'revenue' ? direction : null}
-                  onClick={this.handleSort('revenue')}
-                >
-                  Unit Sales
-                </Table.HeaderCell>
-                <Table.HeaderCell
-                  sorted={column === 'variableCosts' ? direction : null}
-                  onClick={this.handleSort('variableCosts')}
-                >
-                  Variable Costs
-                </Table.HeaderCell>
-                <Table.HeaderCell
-                  sorted={column === 'fixedCosts' ? direction : null}
-                  onClick={this.handleSort('fixedCosts')}
-                >
-                  Fixed Costs
-                </Table.HeaderCell>
-                <Table.HeaderCell
-                  sorted={column === 'totalCosts' ? direction : null}
-                  onClick={this.handleSort('totalCosts')}
-                >
-                  Total Costs
-                </Table.HeaderCell>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body key={uniqueKey()}>
-              {data.map(({ units, profit, revenue, variableCosts, fixedCosts, totalCosts }) => (
-                <Table.Row data-testid='data-table-row' key={`${profit}-${revenue}-${variableCosts}`}>
-                  <Table.Cell>
-                    {formatNumber(units)}
+                  <Table.Cell>{formatNumber(units)}</Table.Cell>
+                  <Table.Cell className={profit < 0 ? "netLoss" : "netGain"}>
+                    {formatProfitOrLoss(profit)}
                   </Table.Cell>
-                  <Table.Cell className={profit < 0 ? 'netLoss' : 'netGain'}>
-                    {this.formatProfitOrLoss(profit)}
-                  </Table.Cell>
-                  <Table.Cell>
-                    ${formatNumber(revenue)}
-                  </Table.Cell>
-                  <Table.Cell>
-                    ${formatNumber(variableCosts)}
-                  </Table.Cell>
-                  <Table.Cell>
-                    ${formatNumber(fixedCosts)}
-                  </Table.Cell>
-                  <Table.Cell>
-                    ${formatNumber(totalCosts)}
-                  </Table.Cell>
+                  <Table.Cell>${formatNumber(revenue)}</Table.Cell>
+                  <Table.Cell>${formatNumber(variableCosts)}</Table.Cell>
+                  <Table.Cell>${formatNumber(fixedCosts)}</Table.Cell>
+                  <Table.Cell>${formatNumber(totalCosts)}</Table.Cell>
                 </Table.Row>
-              ))}
-            </Table.Body>
-          </Table>
-        </Card.Content>
-      </Card>
-    )
-  }
-}
+              )
+            )}
+          </Table.Body>
+        </Table>
+      </Card.Content>
+    </Card>
+  );
+  // }
+};
 
 BreakEvenDataTable.propTypes = {
-  data: PropTypes.arrayOf(PropTypes.object).isRequired
-}
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default BreakEvenDataTable;
