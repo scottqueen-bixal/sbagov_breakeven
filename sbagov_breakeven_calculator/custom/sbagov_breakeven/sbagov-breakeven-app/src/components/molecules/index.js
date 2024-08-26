ca7fd9255473b4c587f38bb8118b615f0f39d759
scottqueen-bixal <scott.queen@bixal.com>
2024-08-26 10:37:10 -0400

    remove unused header and footer components

--- sbagov_breakeven_calculator/custom/sbagov_breakeven/sbagov-breakeven-app/src/components/molecules/index.js
+++ sbagov_breakeven_calculator/custom/sbagov_breakeven/sbagov-breakeven-app/src/components/molecules/index.js
@@ -1,22 +1,17 @@
-import BecAccordion from './accordion/accordion'
-import BreakEvenDataTable from './breakEvenDataTable/breakEvenDataTable'
-import BreakEvenProfileCard from './breakEvenProfileCard/breakEvenProfileCard'
-import BreakEvenResultsCard from './breakEvenResultsCard/breakEvenResultsCard'
-import BreakEvenGraph from './breakEvenGraph/breakEvenGraph'
-import Footer from './footer/footer'
-import Header from './header/header'
-import Hero from './hero/hero'
-import NumbersInputForm from './numbersInputForm/numbersInputForm'
+import BecAccordion from "./accordion/accordion";
+import BreakEvenDataTable from "./breakEvenDataTable/breakEvenDataTable";
+import BreakEvenProfileCard from "./breakEvenProfileCard/breakEvenProfileCard";
+import BreakEvenResultsCard from "./breakEvenResultsCard/breakEvenResultsCard";
+import BreakEvenGraph from "./breakEvenGraph/breakEvenGraph";
+import Hero from "./hero/hero";
+import NumbersInputForm from "./numbersInputForm/numbersInputForm";
 
-export{
+export {
   BecAccordion,
   BreakEvenDataTable,
   BreakEvenGraph,
   BreakEvenProfileCard,
   BreakEvenResultsCard,
-  Footer,
-  Header,
   Hero,
-  NumbersInputForm
-}
-
+  NumbersInputForm,
+};
